aa6a2a073022a123d1bad52526bafea9e9eaa5cd
Julian <julianmaruca93@gmail.com>
2024-12-04 11:17:13 -0300

    Realizamos un juego de consola en python en donde un jugador se enfrenta a enemigos en el espacio

---
+++ main.py
@@ -0,0 +1,59 @@
+import random
+from clases.enemigo import Enemigo
+from clases.jugador import Jugador
+
+
+def main():
+    nombre_jugador = input(
+        "Bienvenido a la aventura en el espacio. Por favor ingresa tu nombre: "
+    )
+    jugador = Jugador(nombre_jugador)
+
+    enemigos = [
+        Enemigo("Alien", 50, 10),
+        Enemigo("Robot", 30, 5),
+        Enemigo("Monstruo", 70, 15),
+    ]
+
+    print("Comienza la aventura!")
+
+    while True:
+        enemigo_actual = random.choice(enemigos)
+        print(f"Te encuentras con un {enemigo_actual.nombre} en tu camino")
+
+        while enemigo_actual.salud > 0:
+            accion = input("Qué deesas hacer? (atacar/huir): ").lower()
+
+            if accion == "atacar":
+                dano_jugador = jugador.atacar()
+                print(
+                    f"Has atacado al {enemigo_actual.nombre} y le has causado {dano_jugador} de daño"
+                )
+                enemigo_actual.recibir_dano(dano_jugador)
+
+                if enemigo_actual.salud > 0:
+                    dano_enemigo = enemigo_actual.atacar()
+                    print(
+                        f"El {enemigo_actual.nombre} te atacó y te causó {dano_enemigo} de daño"
+                    )
+                    jugador.recibir_dano(dano_enemigo)
+
+            elif accion == "huir":
+                print("Has decidido huir del combate")
+                break
+
+        if jugador.salud <= 0:
+            print("Has perdido la partida")
+            break
+
+        jugador.ganar_experiencia(20)
+
+        continuar = input("Quiere seguir explorando (s/n): ").lower()
+
+        if continuar != "s":
+         print("Gracias por haber jugador batallas galácticas")
+         break
+
+if __name__ == "__main__": #nos asegura que solo podremos ejecutar este script desde el archivo principal
+    main()
+
